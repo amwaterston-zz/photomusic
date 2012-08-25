8db95bd89802c0ad9426662c92eda097bbce1bb5
Alex Waterston <alexwaterston@Alexander-Waterstons-MacBookPro.local>
2012-08-25 13:07:18 +0100

    Initial Commit

---
+++ musictures/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  musictures
+//
+//  Created by Alex Waterston on 25/08/2012.
+//  Copyright (c) 2012 Alex Waterston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ musictures/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  musictures
+//
+//  Created by Alex Waterston on 25/08/2012.
+//  Copyright (c) 2012 Alex Waterston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
